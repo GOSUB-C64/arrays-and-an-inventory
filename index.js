e3d904ebff0b00f26e310b5454acc665de62c129
Chris Curran <chriscurran8@yahoo.com>
2023-05-24 10:12:06 +0000

    First commit - setting up array(s)

---
+++ index.js
@@ -0,0 +1,21 @@
+let backpack = [];
+let items = [ 
+    "nail",
+    "bolts",
+    "scrap metal",
+    "9v battery",
+    "padlock"
+];
+<script>
+    let data = prompt("Enter some data:");
+</script>
+
+
+backpack.push(items);
+
+
+
+
+
+console.log(backpack);
+
