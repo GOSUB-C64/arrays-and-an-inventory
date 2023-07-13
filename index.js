bb70fd52f03c3d96672d7f2bce645cab97f29814
Chris Curran <chriscurran8@yahoo.com>
2023-07-13 11:02:17 +0000

    basic setup

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 window.onload = function(){
 
+    init();
+
+};
+
+init=() => {
+
     let rm_description = 'You wake in your bed and get up only to find that your door is locked!!'
     let rm_objects = 'You see a box at your feet.'
     let rm_exits = 'Exits: West'
@@ -7,10 +13,12 @@ window.onload = function(){
     let description = document.getElementsByClassName('description');
     let objects = document.getElementsByClassName('objects');
     let exits = document.getElementsByClassName('exits');
+
     description[0].innerHTML = rm_description;
     objects[0].innerHTML = rm_objects;
     exits[0].innerHTML = rm_exits;
 };
+
 
 
 // const backpack = [];
@@ -22,7 +30,7 @@ window.onload = function(){
 //     "padlock"
 // ];
 
-// let data = prompt("Enter some data:");
+// let data = prompt("what do you want to do now?");
 
 // items.push(data);
 // backpack.push(items);
